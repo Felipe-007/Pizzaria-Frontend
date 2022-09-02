4974f3158e0336986a55b5018b92bd832b657fd9
Felipe <felipe.presidencia@gmail.com>
2022-09-02 10:22:10 -0300

    C3 - Criando document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from "../../node_modules/next/head"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Pizzaria-Frontend</h1>
-    </div>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div>
+        <h1>Pizzaria-Frontend</h1>
+      </div>
+    </>
   )
 }

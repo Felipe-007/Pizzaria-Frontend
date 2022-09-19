c24b7b17daf5d12f4e5892dfd814d0d7950a119c
Felipe <felipe.presidencia@gmail.com>
2022-09-19 09:02:54 -0300

    C26 - Buscando orders (pedidos)

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,8 +3,29 @@ import Head from "next/head"
 import { Header } from "../../components/Header"
 import styles from './styles.module.scss';
 import { FiRefreshCcw } from "react-icons/fi";
+import { setupAPIClient } from "../../services/api";
+import { useState } from 'react';
 
-export default function Dashboard() {
+type OrderProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+}
+
+interface HomeProps {
+  orders: OrderProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps) {
+
+  const [orderList, setOrderList] = useState(orders || [])
+
+  function handleOpenModalView(id: string){
+    alert('Id clicado' + id)
+  }
+
   return (
     <>
       <Head>
@@ -12,35 +33,42 @@ export default function Dashboard() {
       </Head>
       <div>
         <Header />
-        
+
         <main className={styles.container}>
 
-        <div className={styles.containerHeader}>
-          <h1>Últimos pedidos</h1>
-          <button>
-            <FiRefreshCcw size={25} color="#3fffa3"/>
-          </button>
-        </div>
-
-        <article className={styles.listOreders}>
-
-          <section className={styles.orderItem}> 
+          <div className={styles.containerHeader}>
+            <h1>Últimos pedidos</h1>
             <button>
-              <div className={styles.tag}></div>
-              <span>Mesa 30</span>
+              <FiRefreshCcw size={25} color="#3fffa3" />
             </button>
-          </section>                  
+          </div>
 
-        </article>
+          <article className={styles.listOreders}>
 
-      </main>
+            {orderList.map( item => (
+              <section className={styles.orderItem}>
+                <button onClick={ () => handleOpenModalView(item.id) }>
+                  <div className={styles.tag}></div>
+                  <span>Mesa {item.table}</span>
+                </button>
+              </section>
+            ))}
+          </article>
+        </main>
       </div>
     </>
   )
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+
+  const response = await apiClient.get('/orders');
+  //console.log(response.data)
+
   return {
-    props: {}
+    props: {
+      orders: response.data
+    }
   }
 })
